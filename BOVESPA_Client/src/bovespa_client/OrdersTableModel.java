8764b2137dd27fc6b009b6e9ee979de94ce06f76
Aline Borges <alinekborges@gmail.com>
2017-10-10 10:19:31 -0300

    Added missing files

---
+++ BOVESPA_Client/src/bovespa_client/OrdersTableModel.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bovespa_client;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author alinekborges
+ */
+public class OrdersTableModel extends AbstractTableModel {
+    
+    public ArrayList<Order> orders;
+            
+    public OrdersTableModel(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public int getRowCount() {
+        return orders.size();
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        if (col == 0) { return "TYPE"; }
+        if (col == 1) { return "NAME"; }
+        if (col == 2) { return "PRICE"; }
+        if (col == 3) { return "STATUS"; }
+        
+        return "";
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Order order = orders.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return order.type;
+            case 1:
+                return order.stock;
+            case 2:
+                return order.price;
+            case 3:
+                return order.status;
+            default:
+                return "";
+        }
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+}
